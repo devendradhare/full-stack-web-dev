f5b6fea1bb1c22d429a98c7d12a6f6d53c8ef218
devendra dhare <devendradhare03@gmail.com>
2023-06-13 12:46:11 +0530

    assignment 02 is complete

---
+++ assignment 02/assignment_02_solution.py
@@ -0,0 +1,46 @@
+"""
+        Assignment-2: Fundamentals of Python
+
+
+1.  Write a command to get the Python version you are using.
+ans-> To get the Python version using the command prompt (CMD) in Windows, you can follow these steps:
+    1. Open the command prompt by pressing the Windows key + R, type "cmd" in the Run dialog box, and hit Enter.
+    2. In the command prompt, type the following command and press Enter:
+    python --version
+    This command will display the Python version installed on your system.
+    Note: If you have multiple versions of Python installed, you may need to specify the version explicitly 
+    by using the specific Python executable in the command. For example, if you have Python 3 installed, 
+    you can use the command `python3 --version` to get the Python 3 version.
+
+    
+
+2.  What is PVM?
+ans-> PVM stands for Python Virtual Machine. It is the runtime engine of Python that executes Python bytecode. 
+      The PVM is responsible for interpreting and executing the compiled bytecode generated from Python source code.
+
+      
+
+3.  How to create .pyc (Byte Code) file from python source file?
+ans-> Python automatically creates .pyc (byte code) files when you import modules or execute Python scripts. 
+    However, if you specifically want to create .pyc files without executing the code, 
+    you can use the `py_compile` module. Run the following command in the command prompt or terminal:
+    python -m py_compile your_file.py
+    This will generate a .pyc file for the given Python source file.
+
+    
+
+4.  How to run a Python file from command prompt or terminal?
+ans-> To run a Python file from the command prompt or terminal, use the following command:
+    python your_file.py
+    Make sure you have Python installed and added to your system's PATH variable. 
+    Replace "your_file.py" with the actual filename and path if it's in a different directory.
+
+    
+
+5.  "Data Types in Python are not keywords" - True or False
+ans-> False. Data types in Python, such as `int`, `str`, `float`, and `bool`, are keywords in Python. 
+    Keywords are reserved words that have a predefined meaning in the language and cannot be used as 
+    variable names or identifiers.
+
+
+"""
