402ab3ed489e2873801ce9e66e733f311a552c98
devendra dhare <devendradhare03@gmail.com>
2023-06-13 12:44:47 +0530

    assignment 01 is complete

---
+++ assignment 01/assignment_01_solution.py
@@ -0,0 +1,33 @@
+"""
+
+                Assignment-I: Warmup
+
+I.    Who developed Python?
+ans-> Python was developed by Guido van Rossum.
+    
+2.    When was the first version of Python launched?
+ans-> The first version of Python, Python 0.9.0, was released in February 1991.
+    
+3.    "Python is a platform independent language" - True or False
+ans-> True. Python is a platform-independent language, 
+      meaning that Python programs can run on different operating systems 
+      without requiring any changes to the code.
+    
+4.    How to make single line comment in a python file?
+ans-> In Python, a single line comment can be made by starting the line 
+      with the hash symbol (#). Anything written after the hash symbol on that line will be considered a comment and will be ignored by the interpreter.
+    
+5.    "Python is a statically typed programming language" - True or False
+ans-> False. Python is a dynamically typed programming language. 
+      This means that variables do not have predefined types, 
+      and their types can be changed at runtime.
+    
+
+"""
+
+
+
+
+
+
+
